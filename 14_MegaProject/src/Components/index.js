535d73de99237ada7b2ea3e4db482384ef615f70
Bishuthapa <thapabishu435@gmail.com>
2025-01-25 18:44:55 +0545

    react

---
+++ 14_MegaProject/src/Components/index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./Logo";
+import Container from "./container/Container";
+import Button from "./Button";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import PostForm from "./Post-form/PostForm";
+import PostCard from "./postCard";
+import Input from "./Input";
+import AuthLayout from "./AuthLayout";
+import Select from "../Select";
+import Login from "./Login";
+
+export {
+  Header,
+  Footer,
+  LogoutBtn,
+  Logo,
+  Input,
+  Container,
+  Button,
+  RTE,
+  Signup,
+  PostForm,
+  PostCard,
+  AuthLayout,
+  Select,
+  Login,
+};
--- Bishesh0/src/Components/Home/Home.jsx
+++
@@ -1,8 +0,0 @@
-export const Home = () => {
-    return(
-        <>
-        <h1 className="w-screen h-screen bg-black flex justify-center text-white">Home</h1>
-      
-        </>
-    )
-}
